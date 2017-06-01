a9e0d1b73b6aab695c398b8af1e0c15f102c49b3
byzg <byzg@mail.ru>
2017-06-01 16:14:01 +0300

    Create todo component

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  title = 'Todo list';
+  items: string[] = [
+    'To drink',
+    'To eat',
+    'To sleep'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
