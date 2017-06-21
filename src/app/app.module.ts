8ec82e098b9200116c5e6569d1b77578ca5cf47d
byzg <byzg00@gmail.com>
2017-06-21 23:08:10 +0300

    Delete button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdToolbarModule, MdListModule, MdCheckboxModule } from '@angular/material';
+import {
+  MdToolbarModule, MdListModule, MdCheckboxModule, MdIconModule, MdButtonModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
@@ -21,7 +23,9 @@ import { TodoComponent } from './todo/todo.component';
     BrowserAnimationsModule,
     MdToolbarModule,
     MdListModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
